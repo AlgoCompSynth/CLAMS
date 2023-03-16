783be91aa75ca0c1d7908d8f7d522ab00a6bcd44
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-03-15 17:44:02 -0700

    switch to fork of pico-extras

--- linux-pico-tools/0fresh-source.sh
+++ linux-pico-tools/0fresh-source.sh
@@ -36,37 +36,37 @@ pushd $PICO_PATH
 echo ""
 echo "Downloading Pico SDK"
 sleep 5
-git clone https://github.com/raspberrypi/pico-sdk.git --recurse-submodules
+git clone https://github.com/raspberrypi/pico-sdk.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico examples"
 sleep 5
-git clone https://github.com/raspberrypi/pico-examples.git --recurse-submodules
+git clone https://github.com/raspberrypi/pico-examples.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico extras"
 sleep 5
-git clone https://github.com/raspberrypi/pico-extras.git --recurse-submodules
+git clone git@github.com:AlgoCompSynth/pico-extras.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico playground"
 sleep 5
-git clone https://github.com/raspberrypi/pico-playground.git --recurse-submodules
+git clone git@github.com:AlgoCompSynth/pico-playground.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pimoroni examples"
 sleep 5
-git clone -b main git@github.com:AlgoCompSynth/pimoroni-pico.git --recurse-submodules
+git clone -b main git@github.com:AlgoCompSynth/pimoroni-pico.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading OpenOCD"
 sleep 5
-git clone https://github.com/raspberrypi/openocd.git --branch rp2040 --recursive --depth=1
+git clone https://github.com/raspberrypi/openocd.git --branch rp2040 --recursive --depth=1 --quiet
 
 echo ""
 echo "Downloading picotool"
 sleep 5
-git clone https://github.com/raspberrypi/picotool.git --branch master
+git clone https://github.com/raspberrypi/picotool.git --branch master --quiet
 
 popd
 
