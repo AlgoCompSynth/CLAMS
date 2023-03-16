89267a0b48627a1731a74420f6e8c4fe4d793f0f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-03-15 18:18:06 -0700

    Revert "don't nuke all the other repos!"
    This reverts commit 616344509a6057ba2e2bc49bb105d5afaff1d14c.

--- linux-pico-tools/0fresh-source.sh
+++ linux-pico-tools/0fresh-source.sh
@@ -5,6 +5,10 @@ set -e
 source set_pico_envars
 
 echo ""
+echo "Removing existing Pico tools!"
+rm -fr $PICO_PATH; mkdir --parents $PICO_PATH
+
+echo ""
 echo "Installing Linux build tools"
 sudo apt-get update
 sudo apt-get -qqy upgrade
@@ -32,43 +36,36 @@ pushd $PICO_PATH
 echo ""
 echo "Downloading Pico SDK"
 sleep 5
-rm -fr pico-sdk
 git clone https://github.com/raspberrypi/pico-sdk.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico examples"
 sleep 5
-rm -fr pico-examples
 git clone https://github.com/raspberrypi/pico-examples.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico extras"
 sleep 5
-rm -fr pico-extras
 git clone git@github.com:AlgoCompSynth/pico-extras.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pico playground"
 sleep 5
-rm -fr pico-playground
 git clone git@github.com:AlgoCompSynth/pico-playground.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading Pimoroni examples"
 sleep 5
-rm -fr pimoroni-pico
 git clone -b main git@github.com:AlgoCompSynth/pimoroni-pico.git --recurse-submodules --quiet
 
 echo ""
 echo "Downloading OpenOCD"
 sleep 5
-rm -fr openocd
 git clone https://github.com/raspberrypi/openocd.git --branch rp2040 --recursive --depth=1 --quiet
 
 echo ""
 echo "Downloading picotool"
 sleep 5
-rm -fr picotool
 git clone https://github.com/raspberrypi/picotool.git --branch master --quiet
 
 popd
