50fe02b389444c49c20c7d10bdafce5a14626514
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-26 13:20:34 -0700

    fully parameterized!
    Former-commit-id: 0f02d95cfe8490dd65123f28dfac3d0bc47f8954

--- PicoVision-SDK-setup/pico-toolchain.sh
+++ PicoVision-SDK-setup/pico-toolchain.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-echo "Copying picoprobe udev rules"
-sudo cp 99-pico.rules /etc/udev/rules.d/99-pico.rules
-
 echo "Setting up Pico SDK"
-pushd $HOME/Projects
+source set_pico_envars
+pushd $PICO_HOME
 curl -sOL https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
 chmod +x pico_setup.sh
 ./pico_setup.sh
+popd
+
+echo "Copying picoprobe udev rules"
+sudo cp 99-pico.rules /etc/udev/rules.d/99-pico.rules
 
 echo "Finished!"
