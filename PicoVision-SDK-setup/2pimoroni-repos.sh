0bf29ad104279f468cdd602d38ac20b76c14d53f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-26 13:25:53 -0700

    parameterize Pimoroni downloads
    Former-commit-id: a129d5eab9f1d2e16ca0690eae098884cc5e1840

--- PicoVision-SDK-setup/2pimoroni-repos.sh
+++ PicoVision-SDK-setup/2pimoroni-repos.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-pushd $HOME/Projects/pico
+source set_pico_envars
+pushd $PICO_PATH
 
 echo "Downloading Pimoroni examples"
 git clone -b main https://github.com/pimoroni/pimoroni-pico.git --recurse-submodules
@@ -12,9 +13,6 @@ git clone -b main https://github.com/pimoroni/picovision.git --recurse-submodule
 
 echo "Downloading Pimoroni picovision-boilerplate"
 git clone -b main https://github.com/pimoroni/picovision-boilerplate.git --recurse-submodules
-
-echo "Downloading Pimoroni picovision-demos"
-git clone -b main https://github.com/technolhodgy/picovision-demos.git --recurse-submodules
 
 echo "Downloading Pimoroni picovision-projects"
 git clone -b main https://github.com/MichaelBell/picovision-projects.git --recurse-submodules
