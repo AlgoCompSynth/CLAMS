6c3892584c1ff73610a70d4e31f719cfc258f97d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-26 23:51:25 -0700

    combine C/C++ and MicroPython downloads; add README
    Former-commit-id: a3ffcedba08af8c00765c1997b71db2affd4531b

---
+++ PicoVision-dev-setup/4MicroPython.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+source set_pico_envars
+
+echo "Downloading MicroPython release $PICOVISION_VERSION"
+curl -sOL \
+  "https://github.com/pimoroni/picovision/releases/download/v$PICOVISION_VERSION/pimoroni-picovision-v$PICOVISION_VERSION-micropython.uf2"
+curl -sOL \
+  "https://github.com/pimoroni/picovision/releases/download/v$PICOVISION_VERSION/pimoroni-picovision-widescreen-v$PICOVISION_VERSION-micropython.uf2"
+
+echo "Downloading flash_nuke.uf2"
+curl -sOL https://datasheets.raspberrypi.com/soft/flash_nuke.uf2
+
+echo "Finished!"
