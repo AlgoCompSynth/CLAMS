80370703f86328268376488a7dcf9eafbd9bb669
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-28 20:56:00 -0700

    add link to flash_nuke.uf2 download documentation
    Former-commit-id: ea2be9c0759fa2c90efc40d15cab0dd363993cd5

--- PicoVision-dev-setup/4MicroPython.sh
+++ PicoVision-dev-setup/4MicroPython.sh
@@ -11,6 +11,7 @@ curl -sOL \
   "https://github.com/pimoroni/picovision/releases/download/v$PICOVISION_VERSION/pimoroni-picovision-widescreen-v$PICOVISION_VERSION-micropython.uf2"
 
 echo "Downloading flash_nuke.uf2"
+# https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html#resetting-flash-memory
 curl -sOL https://datasheets.raspberrypi.com/soft/flash_nuke.uf2
 
 echo "Finished!"
