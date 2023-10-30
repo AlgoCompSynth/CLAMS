1952f8d65922fad37cef91168a2e595fdb375925
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-29 17:01:16 -0700

    time and log the install / build

--- PicoVision-dev-setup/1pico-toolchain.sh
+++ PicoVision-dev-setup/1pico-toolchain.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+echo "Installing system '/usr/bin/time' command if needed"
+sudo apt-get install -qqy --no-install-recommends time
+
 echo "Setting up Pico SDK"
 source set_pico_envars
 pushd $PICO_HOME
 curl -sOL https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
 chmod +x pico_setup.sh
-./pico_setup.sh
+/usr/bin/time ./pico_setup.sh 2>&1 | tee pico_setup.log
 popd
 
 echo "Copying picoprobe udev rules"
