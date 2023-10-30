54bd686e8805390d3fd5f8117e98f171893ac44e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-29 18:01:08 -0700

    install python3-venv and mpremote venv
    Former-commit-id: ec27bce3b7f9d6d1a64a5d0ef210bd6eec9ef69c

--- PicoVision-dev-setup/4MicroPython.sh
+++ PicoVision-dev-setup/4MicroPython.sh
@@ -14,4 +14,17 @@ echo "Downloading flash_nuke.uf2"
 # https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html#resetting-flash-memory
 curl -sOL https://datasheets.raspberrypi.com/soft/flash_nuke.uf2
 
+echo "Installing 'python3-venv' if needed"
+sudo apt-get install -qqy --no-install-recommends python3-venv
+
+echo "Creating fresh 'mpremote' virtual environment"
+python3 -m venv mpremote --clear
+source ./mpremote/bin/activate
+pip install --upgrade mpremote
+echo "Testing mpremote"
+mpremote connect list
+
+echo "To use 'mpremote', type 'source ./mpremote/bin/activate'"
+sleep 5
+
 echo "Finished!"
