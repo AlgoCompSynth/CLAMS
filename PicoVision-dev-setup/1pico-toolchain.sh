6940925b5d8ac632fe6303366a78fc737ef013f7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-10-31 16:23:41 -0700

    download Rapsberry Pi Debug Probe firmware

--- PicoVision-dev-setup/1pico-toolchain.sh
+++ PicoVision-dev-setup/1pico-toolchain.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Installing command line utilities"
-sudo apt-get install -qqy --no-install-recommends
+sudo apt-get install -qqy --no-install-recommends \
   minicom \
   time \
   tree \
@@ -23,5 +23,15 @@ sudo cp 99-pico.rules /etc/udev/rules.d/99-pico.rules
 echo "Downloading pinout diagram"
 curl -sL \
   "https://cdn.shopify.com/s/files/1/0174/1800/files/picovision_diagram.pdf?v=1696414342" > picovision_diagram.pdf
+
+echo Downloading latest debug probe firmware!
+curl -sOL \
+  "https://github.com/raspberrypi/picoprobe/releases/download/picoprobe-cmsis-v1.0.3/debugprobe.uf2"
+
+if [ -f $HOME/.zshrc ]
+then
+  echo "Adding Pico envars to $HOME/.zshrc"
+  grep -e 'mkdir' -v set_pico_envars >> $HOME/.zshrc
+fi
 
 echo "Finished!"
