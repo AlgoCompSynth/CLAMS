b29cd1dd7f6576b766aca00659329a825f0c1cdc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-11-03 00:20:49 -0700

    testing 64 bit C math in Pico

--- CLAMS-Forth/make-uf2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-rm -fr build; mkdir build; pushd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make --jobs=`nproc`
-ls -Altr
-popd
---
+++ CLAMS-Forth/run-host.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+set -e
+
+gcc -o CLAMS-Forth CLAMS-Forth.c
+./CLAMS-Forth
