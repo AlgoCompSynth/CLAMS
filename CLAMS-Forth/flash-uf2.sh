fc0a3a14ef0f4555845646762e421b0921432b8b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-11-02 21:16:45 -0700

    renaming
    Former-commit-id: c7fd8dcb2d79744c419223dec538de325dbccbf9

--- CLAMS-Forth/flash-uf2.sh
+++ CLAMS-Forth/flash-uf2.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ `df | grep -e '/media/znmeb/RPI-RP2' | wc -l` -gt "0" ]
 then
-  cp build/CLAMS16.uf2 /media/znmeb/RPI-RP2/
+  cp build/CLAMS-Forth.uf2 /media/znmeb/RPI-RP2/
   exit 0
 else
   echo "No RPI-RP2 USB Drive!!"
