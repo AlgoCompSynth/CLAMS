12a4c119569d79a60ce63306a78ff4890279a50f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-11-13 16:32:42 -0800

    start coding stdio words
    Former-commit-id: 1e6eadffc3d640bb4eb7713815672c15a90fef66

--- CLAMS-Forth/Wrappers.c
+++ CLAMS-Forth/Wrappers.c
@@ -1,6 +1,57 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+/*
 
-void pico_sleep_ms(uint32_t ms) {
+  ## License
+
+  MIT License
+
+  Copyright (c) 2023 M. Edward (Ed) Borasky
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+// C wrappers for Pico SDK functions
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "pico/stdlib.h"
+#include "pico/stdio/driver.h"
+#include "pico/stdio.h"
+#include "pico/stdio_usb.h"
+#include "pico/stdio_uart.h"
+
+void sdk_sleep_ms(uint32_t ms) {
   sleep_ms(ms);
+}
+
+void sdk_enable_uart () {
+  stdio_set_driver_enabled(&stdio_uart, true);
+}
+
+void sdk_disable_uart () {
+  stdio_set_driver_enabled(&stdio_uart, false);
+}
+
+void sdk_enable_usb () {
+  stdio_set_driver_enabled(&stdio_usb, true);
+}
+
+void sdk_disable_usb () {
+  stdio_set_driver_enabled(&stdio_usb, false);
 }
