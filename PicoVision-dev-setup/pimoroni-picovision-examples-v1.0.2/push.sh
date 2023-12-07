630b70900b7567bb7357c97fdbc55a5e3638cb5d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-12-06 16:58:42 -0800

    add new examples

--- PicoVision-dev-setup/pimoroni-picovision-examples-v1.0.2/push.sh
+++ PicoVision-dev-setup/pimoroni-picovision-examples-v1.0.2/push.sh
@@ -2,21 +2,25 @@
 
 set -e
 
-source ../mpremote/bin/activate
+#source ../mpremote/bin/activate
 mpremote ls
 mpremote cp -r alert/ :
+mpremote cp -r basic/ :
 mpremote cp -r bouncing_logo/ :
+mpremote cp -r co2/ :
 mpremote cp -r floppy_birb/ :
+mpremote cp -r magic_mirror/ :
 mpremote cp -r pride/ :
 mpremote cp -r sneks_and_ladders/ :
-mpremote cp main.py :
-mpremote cp rainbow_wheel.py :
-mpremote cp screenmodes.py :
-mpremote cp scrollgroups.py :
-mpremote cp starfield.py :
-mpremote cp starfield_rainbow.py :
+mpremote cp *.py :
 mpremote cp toaster.png :
-mpremote cp vector_clock.py :
-mpremote cp vector_clock_smooth.py :
 mpremote ls
+mpremote ls :alert
+mpremote ls :basic
+mpremote ls :bouncing_logo
+mpremote ls :co2
+mpremote ls :floppy_birb
+mpremote ls :magic_mirror
+mpremote ls :pride
+mpremote ls :sneks_and_ladders
 mpremote reset
