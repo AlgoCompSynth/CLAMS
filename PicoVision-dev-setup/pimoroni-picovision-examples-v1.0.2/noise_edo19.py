ce280e4a6da907e2593d94b91b55cb1e65564b1f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-12-09 20:16:54 -0800

    19-EDO scales and chords => comments!
    Former-commit-id: 5f42f37c646afa7c15c2725ebf8cc0443b9b83cf

--- PicoVision-dev-setup/pimoroni-picovision-examples-v1.0.2/noise_edo19.py
+++ PicoVision-dev-setup/pimoroni-picovision-examples-v1.0.2/noise_edo19.py
@@ -107,15 +107,76 @@ TONES = {
 }
 
 # do 19-EDO calculations
-base_frequency = 130.8128
+base_frequency = 130.8128 # an octave below middle C
 notes_per_octave = 19
 ratio = math.pow(2.0, (1.0 / notes_per_octave))
 frequencies = [base_frequency]
-rounded_frequencies = [round(base_frequency)]
+
+# PicoSynth uses 16-bit integer frequencies :-(
+rounded_frequencies = [round(base_frequency)] 
 
 for index in range(1, 2 * notes_per_octave + 1):
     frequencies.append(ratio * frequencies[index - 1])
     rounded_frequencies.append(round(frequencies[index]))
+
+# named 19-EDO scales from https://en.xen.wiki/w/Strictly_proper_19edo_scales
+## 3 3 3 3 3 4 Deutone[6]
+## 1 3 3 3 3 3 3 Deutone[7]
+## 2 2 3 3 3 3 3 Major Locrian
+## 2 3 2 3 3 2 4 Harmonic Minor
+## 2 3 2 3 3 3 3 Melodic Minor
+## 2 3 2 4 2 3 3 Harmonic Major
+## 2 3 3 2 3 3 3 Meantone[7]
+## 2 2 3 2 2 3 2 3 Sensi[8]
+## 2 2 2 2 2 2 2 2 3 Negri[9]
+## 1 2 2 2 2 2 2 2 2 2 Negri[10]
+
+# named 19-EDO chords from https://en.xen.wiki/w/19edo_chords
+
+## Triads
+### Chord name | Symbol | Notes | Steps | Cents
+### Major | C | C E G | 0-6-11 | 0-379-695
+### Minor | Cm, C- | C Eb G | 0-5-11 | 0-316-695
+### SuperMajor | Csmaj | C E# G | 0-7-11 | 0-442-695
+### SubMinor | Csmin | C Ebb G | 0-4-11 | 0-253-695
+### sus4 | Csus4 | C F G | 0-8-11 | 0-505-695
+### sus2 | Csus2 | C D G | 0-3-11 | 0-189-695
+### Diminished | Cdim, C° | C Eb Gb | 0-5-10 | 0-316-632
+### Augmented | Caug, C+ | C E G# | 0-6-12 | 0-379-758
+
+## Tetrads - Major chords
+### Major seventh | Cmaj7 | C E G B | 0-6-11-17 | 0-379-695-1074
+### Dominant seventh | C7 | C E G Bb | 0-6-11-16 | 0-379-695-1011
+### Harmonic seventh | Ch7 | C E G Bbb | 0-6-11-15 | 0-379-695-947
+### Sixth | C6 | C E G A | 0-6-11-14 | 0-379-695-884
+
+## Tetrads - Minor chords
+### Minor seventh | Cm7 | 0-5-11-16 | 0-316-695-1011
+### Minor major seventh | Cmmaj7 | 0-5-11-17 | 0-316-695-1074
+### Minor augmented six | Cm+6 | 0-5-11-15 | 0-316-695-947
+### Minor six | Cm6 | 0-5-11-14 | 0-316-695-884
+### Minor seven flat six (NT aeolian seven) | Cm7(b6) [Faeol7] | 0-5-13-16 | 0-316-821-1011
+
+## Tetrads - Supermajor chords
+### Supermajor seventh | Csmaj7 | 0-7-11-18 | 0-442-695-1137
+
+## Tetrads - Subminor chords
+### Subminor seventh | Csmin7 | 0-4-11-15 | 0-253-695-947
+
+### Tetrads - Diminished chords
+### Diminished seven (fully diminished) | Cdim7, C°7 | 0-5-10-15 | 0-316-632-947
+### Minor seven flat five (half-diminished) | Cm7(b5), Cø7 | 0-5-10-16 | 0-316-632-1011
+
+## Tetrads - Augmented chords
+### Augmented seven | Caug7, C+7, C7#5 | 0-6-12-16 | 0-379-758-1011
+### Major seven sharp five | Cmaj7#5 | 0-6-12-18 | 0-379-758-1137
+
+## Pentads (ninth chords) - Major chords
+### Major ninth | Cmaj9 | 0-6-11-17-22 | 0-379-695-1074-1389
+### Dominant ninth | C9 | 0-6-11-16-22 | 0-379-695-1011-1389
+### Dominant seven flat nine | C7(b9) | 0-6-11-16-21 | 0-379-695-1011-1326
+### Harmonic ninth | Ch9 | 0-6-11-15-22 | 0-379-695-947-1389
+### Harmonic seven flat nine | Ch7(b9) | 0-6-11-15-21 | 0-379-695-947-1326
 
 # initialize synthesizer
 synth = PicoSynth()
@@ -127,7 +188,7 @@ noise = synth.channel(0)
 # waveforms you can use are NOISE, SQUARE, SAW, TRIANGLE, SINE, or WAVE
 # you can combine more than one, like this: waveforms=Channel.SQUARE | Channel.SAW,
 noise.configure(
-    waveforms=Channel.SINE,
+    waveforms=Channel.TRIANGLE,
     attack=0.1,
     decay=0.1,
     sustain=0.5,
@@ -170,4 +231,4 @@ while True:
 
         noise.frequency(note)
         noise.trigger_attack()
-        time.sleep(1.0)
+        time.sleep(0.5)
