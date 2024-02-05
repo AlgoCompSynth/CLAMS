de519b03fa02eb29dfb6c6c88f39a0899a99d818
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-05 11:39:43 -0800

    test Arch build chain
    Former-commit-id: 3c91e72278f3e841d9844c6e72a6790af43b9fda

--- linux-pico-tools/0fresh-source.sh
+++ linux-pico-tools/0fresh-source.sh
@@ -8,29 +8,6 @@ echo ""
 echo "Removing existing Pico tools!"
 rm -fr $PICO_PATH; mkdir --parents $PICO_PATH
 
-echo ""
-echo "Installing Linux build tools"
-sudo apt-get update
-sudo apt-get -qqy upgrade
-sudo apt-get -qqy install \
-  autoconf \
-  automake \
-  build-essential \
-  ca-certificates \
-  cmake \
-  gcc-arm-none-eabi \
-  git \
-  libftdi-dev \
-  libnewlib-arm-none-eabi \
-  libstdc++-arm-none-eabi-newlib \
-  libtool \
-  libusb-1.0-0-dev \
-  ninja-build \
-  software-properties-common \
-  texinfo \
-  time \
-  tree
-
 pushd $PICO_PATH
 
 echo ""
