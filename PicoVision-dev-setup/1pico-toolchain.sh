8860125337046650e360feed035951f314b3da01
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-17 06:48:03 +0000

    tested with distrobox container

--- PicoVision-dev-setup/1pico-toolchain.sh
+++ PicoVision-dev-setup/1pico-toolchain.sh
@@ -3,16 +3,19 @@
 set -e
 
 echo "Installing command line utilities"
-sudo apt-get install -qqy --no-install-recommends \
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
   info \
+  libusb-dev \
   minicom \
   obs-studio \
-  time \
-  tree \
-  vim-nox
+  pkg-config
 
 echo "Setting up Pico SDK"
 source set_pico_envars
+export SKIP_UART=1
+export SKIP_VSCODE=1
 pushd $PICO_HOME
 rm -fr $PICO_PATH
 curl -sOL https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
