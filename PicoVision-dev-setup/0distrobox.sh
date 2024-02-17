683a83d86549d6ce71a862f7f5fe919e667ee7b8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-17 06:15:46 +0000

    Clone Jammy container

--- PicoVision-dev-setup/0distrobox.sh
+++ PicoVision-dev-setup/0distrobox.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-mkdir --parents $HOME/.local/bin
-distrobox rm --force CLAMS-Toolchain
+distrobox rm -f CLAMS-development
 distrobox create \
-  --image quay.io/toolbx-images/ubuntu-toolbox:22.04 \
-  --name CLAMS-Toolchain \
+  --clone Jammy \
+  --name CLAMS-development \
   --init \
-  --additional-packages "systemd libpam-systemd libusb-dev git git-lfs time vim-nox pkg-config zsh-syntax-highlighting zsh-theme-powerlevel9k" \
+  --additional-packages "systemd libpam-systemd git git-lfs time tree vim-nox zsh zsh-autosuggestions zsh-syntax-highlighting" \
   --nvidia
