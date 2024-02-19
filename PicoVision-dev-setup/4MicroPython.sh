501480edbd895698f4ca4e0987ee9d4925ec4462
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-19 14:09:59 -0800

    Tested on Silverblue
    Former-commit-id: 7f5fa81a7d209b24506d3df69bb40415a9afe219

--- PicoVision-dev-setup/4MicroPython.sh
+++ PicoVision-dev-setup/4MicroPython.sh
@@ -4,15 +4,16 @@ set -e
 
 source set_pico_envars
 
-echo "Downloading MicroPython release $PICOVISION_VERSION"
+echo "Downloading PicoVision MicroPython"
 curl -sOL $PICOVISION_PYTHON_DOWNLOAD
 curl -sOL $PICOVISION_PYTHON_WIDESCREEN_DOWNLOAD
+echo "Downloading CircuitPython"
+curl -sOL $CIRCUITPYTHON_DOWNLOAD
+echo "Downloading Base MicroPython"
+curl -sOL $MICROPYTHON_DOWNLOAD
 
 echo "Downloading flash_nuke.uf2"
 # https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html#resetting-flash-memory
 curl -sOL https://datasheets.raspberrypi.com/soft/flash_nuke.uf2
-
-# run separate script for mpremote virtual environment
-./install-mpremote.sh
 
 echo "Finished!"
