2cff5012a69fad9d678555a471d720faf474eec5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-21 12:41:12 -0800

    separate host and distrobox mpremote venvs

--- RPI-Pico-W-Dev-Setup/01host-install-mpremote.sh
+++ RPI-Pico-W-Dev-Setup/01host-install-mpremote.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Creating fresh mpremote venv"
-python3 -m venv --clear mpremote
+python3 -m venv --clear host-mpremote
 
 echo "Activating mpremote venv"
-source mpremote/bin/activate
+source host-mpremote/bin/activate
 
 echo "Installing mpremote"
 pip install --upgrade pip
