a54750211bcd12983b350c97f80aac4fb96024cd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-02-23 15:10:50 -0800

    starting distrobox toolbox
    Former-commit-id: e188d6c998b7dc0dd705f1e5bb370db20fc81580

---
+++ RPI-Pico-W-Dev-Setup/CLAMS-distrobox/00create-distrobox.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+rm -fr $HOME/CLAMS-distrobox
+distrobox rm -f CLAMS
+distrobox create \
+  --image docker.io/library/debian:bookworm-backports \
+  --name CLAMS \
+  --home $HOME/CLAMS-distrobox \
+  --init \
+  --additional-packages \
+    "systemd libpam-systemd apt-file build-essential cmake git git-lfs mlocate ninja-build python3-venv time tree vim-nox"
+cp -rp $HOME/.ssh $HOME/CLAMS-distrobox
+
+distrobox list
