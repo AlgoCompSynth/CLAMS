3f6e4e54dfc1b6e6bae3d8c4a0ca3f034d8fa7ca
Zorro Notorious MEB <znmeb@CLAMS.algocompsynth>
2024-03-13 00:07:07 +0000

    add openocd build
    Former-commit-id: e0da7ff9c816788f8fe98376e17362c3b47730e8

---
+++ CLAMS-distrobox/10command-line.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+set -e
+
+echo "Setting bash aliases"
+cat bash_aliases >> $HOME/.bash_aliases
+source $HOME/.bash_aliases
+
+echo "Upgrading Linux"
+sudo apt-get update -qq && sudo apt-get upgrade -qqy
+sudo apt-get -qqy autoremove
+
+echo "Installing command line tools"
+sudo apt-get install -qqy --no-install-recommends \
+  apt-file \
+  build-essential \
+  cmake \
+  git \
+  git-lfs \
+  locate \
+  man-db \
+  minicom \
+  ninja-build \
+  python3-venv \
+  screen \
+  time \
+  tmux \
+  tree \
+  vim-nox
+
+echo "Updating databases"
+sudo mandb
+sudo updatedb
+sudo apt-file update
+
+echo "Restart bash to activate aliases"
+echo "Finished!!"
