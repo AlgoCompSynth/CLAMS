e0c370790bd921fe1cf121056eeb5a4ba9ba1e27
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-03-20 15:45:23 -0700

    don't re-run existing builds!
    Former-commit-id: 574865722a83036018a3441ad0291cbdb73d4abd

--- RPI-Pico-W-Dev-Setup/24test-NuttX-builds.sh
+++ RPI-Pico-W-Dev-Setup/24test-NuttX-builds.sh
@@ -5,28 +5,35 @@ set -e
 source ./set_pico_envars
 pushd $NUTTX_PATH/nuttx
 
-for configuration in `grep raspberrypi-pico $NUTTX_PATH/supported-configurations.txt`
-do
-  echo ""
-  echo "Testing configuration $configuration"
-  result_path_name=$NUTTX_PATH/`echo $configuration | sed 's/:/../g'`
-  echo "...creating fresh $result_path_name"
-  rm -fr $result_path_name; mkdir --parents $result_path_name
+  for configuration in `grep raspberrypi-pico $NUTTX_PATH/supported-configurations.txt`
+  do
+    echo ""
+    echo ""
+    echo "Testing configuration $configuration"
+    result_path_name=$NUTTX_PATH/`echo $configuration | sed 's/:/../g'`
 
-  make distclean || true
-  echo "...configuring"
-  ./tools/configure.sh -l $configuration > $result_path_name/configure.log
-  echo "...compiling and linking"
-  /usr/bin/time make > $result_path_name/make.log || true
+    if [ ! -d "$result_path_name" ]
+    then
+      echo "...creating $result_path_name"
+      mkdir --parents $result_path_name
+  
+      echo "...cleaning"
+      make distclean || true
+      echo "...configuring"
+      ./tools/configure.sh -l $configuration 2>&1 | tee $result_path_name/configure.log
+      echo "...compiling and linking"
+      /usr/bin/time make 2>&1 | tee $result_path_name/make.log || true
+  
+      # make artifacts like Pico SDK makes
+      ln -s nuttx nuttx.elf
+      arm-none-eabi-objdump -d nuttx.elf > nuttx.dis
+  
+      echo "...saving $configuration artifacts to $result_path_name"
+      cp nuttx* $result_path_name/
+      ls -dl $result_path_name/*
+    fi
 
-  # make artifacts like Pico SDK makes
-  ln -s nuttx nuttx.elf
-  arm-none-eabi-objdump -d nuttx.elf > nuttx.dis
-
-  echo "...saving $configuration artifacts to $result_path_name"
-  cp nuttx* $result_path_name/
-  ls -dl $result_path_name/*
-done
+  done
 
 popd
 
