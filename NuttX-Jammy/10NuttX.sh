bf98663b8ed675fafdc902656ab41f5d795bd57a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-04-08 12:57:58 -0700

    reorganized - smoke test ready

--- NuttX-Jammy/10NuttX.sh
+++ NuttX-Jammy/10NuttX.sh
@@ -5,21 +5,21 @@ set -e
 # https://nuttx.apache.org/docs/latest/quickstart/install.html
 source set_pico_envars
 
-echo ""; sleep 2
+echo "Installing crossbuild-essential for targets"
+sudo apt-get install -qqy \
+  crossbuild-essential-arm64 \
+  crossbuild-essential-armhf \
+  crossbuild-essential-amd64 \
+  crossbuild-essential-i386 \
+  crossbuild-essential-riscv64
+
 echo "Installing NuttX dependencies"
-if [ `uname -m` == "x86_64" ]
-then
-  sudo apt-get install -qqy \
-    gcc-multilib \
-    g++-multilib
-fi
 sudo apt-get install -qqy \
   automake \
   binutils-dev \
   bison \
   build-essential \
   flex \
-  gcc-riscv64-unknown-elf \
   gettext \
   genromfs \
   gperf \
@@ -39,12 +39,10 @@ sudo apt-get install -qqy \
   util-linux \
   xxd
 
-echo ""; sleep 2
 echo "Installing KConfig tools"
 sudo apt-get install -qqy \
   kconfig-frontends
 
-echo ""; sleep 2
 echo "Downloading NuttX version $NUTTX_VERSION"
 rm -fr $NUTTX_PATH; mkdir $NUTTX_PATH; pushd $NUTTX_PATH
 
@@ -57,13 +55,11 @@ else
   git clone --branch nuttx-$NUTTX_VERSION https://github.com/apache/nuttx-apps.git apps
 fi
 
-echo ""; sleep 2
 echo "Listing supported configurations onto $NUTTX_PATH/supported-configurations.txt"
 cd nuttx
-./tools/configure.sh -L > $NUTTX_PATH/supported-configurations.txt
+./tools/configure.sh -L | sort -u > $NUTTX_PATH/supported-configurations.txt
 cd ..
 
 popd
 
-echo ""; sleep 2
 echo "Finished!!"
