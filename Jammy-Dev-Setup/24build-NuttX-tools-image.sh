0e9e65f4dac60c9f153f022e8b3715f2962f1209
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-04-07 20:14:10 -0700

    testing with ESP32-S3!!
    Former-commit-id: 1d1631e458918fde6c37903fbfa12bb77b056443

--- Jammy-Dev-Setup/24build-NuttX-tools-image.sh
+++ Jammy-Dev-Setup/24build-NuttX-tools-image.sh
@@ -9,14 +9,14 @@ echo "Builing tools image"
 /usr/bin/time docker build --tag localhost/nuttx-tools-image:latest .
 popd
 
-echo "Making tools container"
-docker run -d --name nuttx-tools-image-container localhost/nuttx-tools-image:latest
+echo "Creating tools container"
+docker create --name nuttx-tools-container localhost/nuttx-tools-image:latest
 sleep 10
 docker ps -a
 
 echo "Copying tools"
 rm -fr $NUTTX_PATH/tools
-docker cp --follow-link nuttx-tools-image-container:/tools $NUTTX_PATH/
+docker cp --follow-link nuttx-tools-container:/tools $NUTTX_PATH/
 sudo chown -R $USER:$USER $NUTTX_PATH/tools
 
 echo "Finisehd!"
