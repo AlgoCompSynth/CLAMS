febdc7d1ada8456b62b39590c60e241d7459e342
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-04-08 18:15:10 -0700

    just extract tools we need from image
    Former-commit-id: f8dbc6329aee3b318b5a79a2d9b00426b5423c6a

--- NuttX-Jammy/20collect-NuttX-tools.sh
+++ NuttX-Jammy/20collect-NuttX-tools.sh
@@ -14,9 +14,9 @@ docker create --name nuttx-tools-container localhost/nuttx-tools-image:latest
 sleep 10
 docker ps -a
 
-echo "Copying tools"
-rm -fr $NUTTX_PATH/tools
-docker cp --follow-link nuttx-tools-container:/tools $NUTTX_PATH/
+echo "Copying xtensa-esp32s3-elf-gcc tools"
+rm -fr $NUTTX_PATH/tools/xtensa-esp32s3-elf-gcc; mkdir --parents $NUTTX_PATH/tools
+docker cp --follow-link nuttx-tools-container:/tools/xtensa-esp32s3-elf-gcc $NUTTX_PATH/tools
 sudo chown -R $USER:$USER $NUTTX_PATH/tools
 
 echo "Removing tools container"
