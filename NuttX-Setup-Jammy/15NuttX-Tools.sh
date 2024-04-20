0b7c171c7155b15a58259cf7c68aeed74b17cf3c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-04-19 20:55:24 -0700

    clean up envar logic

--- NuttX-Setup-Jammy/15NuttX-Tools.sh
+++ NuttX-Setup-Jammy/15NuttX-Tools.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-source set_pico_envars
+echo "Setting CLAMS_BASE to $HOME"
+export CLAMS_BASE=$HOME
+
+source ./set_pico_envars
 
 pushd $NUTTX_PATH/nuttx/tools/ci/docker/linux
+echo ""
 echo "Builing tools image"
 /usr/bin/time docker build --tag localhost/nuttx-tools-image:latest .
 popd
 
+echo ""
 echo "Making tools container"
 docker rm -f nuttx-tools-container || true
 docker create --name nuttx-tools-container localhost/nuttx-tools-image:latest
 sleep 10
 docker ps -a
 
+echo ""
 echo "Copying tools"
 rm -fr $NUTTX_TOOLS
 docker cp nuttx-tools-container:/tools $NUTTX_PATH/
