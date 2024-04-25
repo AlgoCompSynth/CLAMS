dd3db20340e25f53998e26cc6710ceb4ea9f42a8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-04-25 11:55:38 -0700

    Missing header files added
    Former-commit-id: c999c6335a66025bb94a63d0e7e31c4ada355e8d

--- NuttX-Setup-Jammy/20NuttX-Depends.sh
+++ NuttX-Setup-Jammy/20NuttX-Depends.sh
@@ -2,16 +2,15 @@
 
 set -e
 
+echo ""
 echo "Setting CLAMS_BASE to $HOME"
 export CLAMS_BASE=$HOME
 
-echo ""
 echo "Setting environment variables"
 source ./set_pico_envars
 
 # https://nuttx.apache.org/docs/latest/quickstart/install.html
 
-echo ""
 echo "Installing NuttX dependencies"
 sudo apt-get update -qq
 sudo apt-get upgrade -qqy
@@ -37,14 +36,21 @@ sudo apt-get install -qqy --no-install-recommends \
   libtool \
   picocom \
   pkg-config \
+  python3-pip \
+  python3-venv \
   texinfo \
   u-boot-tools \
   util-linux \
   xxd
 
-echo ""
 echo "Installing KConfig tools"
 sudo apt-get install -qqy --no-install-recommends \
   kconfig-frontends
+
+echo "Creating 'nuttx-venv' Python virtual environment'"
+python3 -m venv $NUTTX_VENV
+source $NUTTX_VENV/bin/activate
+pip install --upgrade kconfiglib
+deactivate
 
 echo "Finished!!"
