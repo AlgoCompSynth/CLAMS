7755357b0a59193e46e0bf7b15409cc91eeb55be
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-06-02 23:55:45 -0700

    ready for smoke test

---
+++ ZephyrDistrobox/2LinuxDependencies.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Defining LOGFILE"
+export LOGFILE=$PWD/Logs/LinuxDependencies.log
+
+# https://docs.zephyrproject.org/latest/develop/getting_started/index.html
+echo ""
+echo "Getting CMake repository"
+pushd /tmp
+wget https://apt.kitware.com/kitware-archive.sh
+sudo bash kitware-archive.sh
+popd
+
+echo "Installing Linux dependencies"
+/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
+  ccache \
+  cmake \
+  device-tree-compiler \
+  dfu-util \
+  file \
+  g++-multilib \
+  gcc \
+  gcc-multilib \
+  git \
+  gperf \
+  libmagic1 \
+  libsdl2-dev \
+  make \
+  ninja-build \
+  python3-dev \
+  python3-pip \
+  python3-setuptools \
+  python3-tk \
+  python3-venv \
+  python3-wheel \
+  wget \
+  xz-utils \
+  >> $LOGFILE 2>&1
+
+echo "Checking versions"
+cmake --version
+python3 --version
+dtc --version
+
+echo "Finished"
