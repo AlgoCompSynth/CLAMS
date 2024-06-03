78f587746f28cea71ee712b682bbe10a2153dce1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-06-02 22:07:53 -0700

    first draft of Distrobox

--- ZephyrDistrobox/unminimize.sh
+++ ZephyrDistrobox/unminimize.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+sudo apt-get update -qq
+sudo apt-get upgrade -qqy
 sudo touch /etc/dpkg/dpkg.cfg.d/excludes
-echo "Y" | sudo unminimize > /dev/null 2>&1
+echo "Y" | sudo unminimize 2>&1 | tee unminimize.log
