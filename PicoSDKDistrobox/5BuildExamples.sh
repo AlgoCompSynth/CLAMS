bdcf209daec931ea59effe24054bf463d375f90a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-11 19:08:17 -0700

    install picotool globally

--- PicoSDKDistrobox/5BuildExamples.sh
+++ PicoSDKDistrobox/5BuildExamples.sh
@@ -9,21 +9,11 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo "Downloading the examples"
-pushd $PICO_PATH
-rm -fr $PICO_EXAMPLES_TARBALL $PICO_EXAMPLES_PATH
-wget --quiet $PICO_EXAMPLES_URL
-tar xf $PICO_EXAMPLES_TARBALL
-
-echo "Finished!"
-exit
-
-  pushd $dir
-  echo ""
-  echo "Configuring $dir"
-  rm -fr build; mkdir build; cd build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DPICO_SDK_POST_LIST_DIRS=$PICO_EXTRAS_PATH 2>&1 | tee cmake.log
-  echo "Compiling $dir"
-  /usr/bin/time make -j`nproc` 2>&1 | tee make.log || true
-  popd
-
+pushd $PICO_EXAMPLES_PATH
+echo ""
+echo "Configuring $PICO_EXAMPLES_PATH"
+rm -fr build; mkdir build; cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug 2>&1 | tee cmake.log
+echo "Compiling $PICO_EXAMPLES_PATH"
+/usr/bin/time make -j`nproc` 2>&1 | tee make.log || true
+popd
