77c29e517cd4e33f97d6e5c6f9fd9b8117dcd8ad
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-15 14:53:52 -0700

    ready for refactoring by board
    Former-commit-id: afba33ff91f840f5bd49f2ed494f2ccaafb8b7e8

--- PicoSDK/4LinuxDeps.sh
+++ PicoSDK/4LinuxDeps.sh
@@ -4,13 +4,15 @@ set -e
 
 echo "Installing base packages"
 sudo apt-get install -y \
-  build-essential \
   cmake \
+  g++ \
+  gcc \
   gcc-arm-none-eabi \
   gdb-multiarch \
   libnewlib-arm-none-eabi \
   libstdc++-arm-none-eabi-newlib \
   libusb-1.0-0-dev \
+  make \
   minicom \
   pkg-config
 
