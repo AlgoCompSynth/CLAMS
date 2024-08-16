78dad4926f7ab5052795afbf26a9c2ac1fa0ecdc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-15 20:31:18 -0700

    code for fetching RISC-V cross-compiler

---
+++ PicoSDK/get-riscv-compiler.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -e
+
+echo "Setting CLAMS_BASE to $HOME"
+export CLAMS_BASE=$HOME
+
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+
+echo ""
+echo "Downloading RISC-V cross-compiler tarball"
+mkdir --parents $PICO_UTILITIES
+pushd $PICO_UTILITIES
+rm -f $RISC_V_COMPILER_TARBALL
+/usr/bin/time wget --quiet $RISC_V_COMPILER_URL
+
+echo "Installing to /usr/local/bin"
+/usr/bin/time tar --extract \
+  --file $RISC_V_COMPILER_TARBALL \
+  --directory /usr/local \
+  --strip-components=1 \
+  > extract.log 2>&1
+popd
+
+echo "Finished!"
