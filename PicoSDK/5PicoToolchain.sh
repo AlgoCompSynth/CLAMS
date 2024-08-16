67baf73c289ee5e1362dcccfe174669ffe93c721
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-15 21:45:59 -0700

    ditch Pimoroni stuff now to focus on RP2350!

--- PicoSDK/5PicoToolchain.sh
+++ PicoSDK/5PicoToolchain.sh
@@ -11,44 +11,52 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo ""
-echo "Creating fresh $PICO_PATH"
 pushd $CLAMS_BASE
-rm -fr $PICO_PATH
-mkdir --parents $PICO_PATH
 
-echo ""
-echo "Cloning Raspberry Pi repositories"
-pushd $PICO_PATH
+  echo ""
+  echo "Creating fresh $PICO_PATH"
+  rm -fr $PICO_PATH
+  mkdir --parents $PICO_PATH
 
-for repo in \
-  $PICO_SDK_URL $PICOTOOL_URL $PICO_EXAMPLES_URL $PICO_EXTRAS_URL $PICO_PLAYGROUND_URL \
-  $PIMORONI_PICO_URL $PICOVISION_URL $PICOVISION_PROJECTS_URL
-do
-  git clone --recursive $repo
-done
+  echo ""
+  echo "Cloning Raspberry Pi repositories"
+  pushd $PICO_PATH
 
-echo ""
-echo "Listing supported boards to $SUPPORTED_BOARDS"
-ls -l $PICO_PATH/pico-sdk/src/boards/include/boards > $SUPPORTED_BOARDS
+    for repo in \
+      $PICO_SDK_URL $PICOTOOL_URL $PICO_EXAMPLES_URL $PICO_EXTRAS_URL $PICO_PLAYGROUND_URL
+    do
+      git clone --recursive $repo
+    done
 
-echo ""
-echo "Installing picotool"
+    echo ""
+    echo "Listing supported boards to $SUPPORTED_BOARDS"
+    ls -l $PICO_PATH/pico-sdk/src/boards/include/boards > $SUPPORTED_BOARDS
 
-pushd $PICOTOOL_PATH
-rm -fr build; mkdir build; cd build
-cmake .. 2>&1 | tee cmake.log
-make 2>&1 | tee make.log
-sudo make install
-picotool version
-popd
+    echo ""
+    echo "Installing picotool"
 
-echo ""
-echo Downloading latest debug probe firmware!
-curl -sOL \
-  "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe_on_pico.uf2"
-curl -sOL \
-  "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe.uf2"
+    pushd $PICOTOOL_PATH
+      rm -fr build; mkdir build; cd build
+      cmake .. 2>&1 | tee cmake.log
+      make 2>&1 | tee make.log
+      sudo make install
+      picotool version
+    popd
+
+    echo ""
+    echo Downloading latest debug probe firmware!
+    pushd $PICO_UTILITIES_PATH
+      curl -sOL \
+        "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe_on_pico.uf2"
+      curl -sOL \
+        "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe.uf2"
+
+      echo "Downloading flash_nuke.uf2"
+      # https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html#resetting-flash-memory
+      curl -sOL "https://datasheets.raspberrypi.com/soft/flash_nuke.uf2"
+    popd
+
+  popd
 
 popd
 
