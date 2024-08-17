b4678dade51924aa38dda858ea7116361b07b473
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-16 17:30:57 -0700

    save to logfile
    Former-commit-id: 880e4b3d3de6b7ee7d1d3ca2ef9827ba793ce62c

--- PicoSDK/list-uf2-files.sh
+++ PicoSDK/list-uf2-files.sh
@@ -9,6 +9,6 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-find $PICO_PATH -name '*.uf2' | sort
+find $PICO_PATH -name '*.uf2' | sort > uf2files.log
 
 echo "Finished"
