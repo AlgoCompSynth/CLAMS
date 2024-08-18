ab16467d28f375636ab1e49c2e7f4fc8513f7a9b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-18 15:04:00 -0700

    move RISC-V compiler out of /usr/local!
    Former-commit-id: 8654e38def53cf47f2b1172df76dc4e9fd5860fd

--- PicoSDK/install-risc-v-compiler.sh
+++ PicoSDK/install-risc-v-compiler.sh
@@ -9,20 +9,19 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo ""
-echo "Downloading RISC-V cross-compiler tarball"
+mkdir --parents $CLAMS_COMPILERS
+pushd $CLAMS_COMPILERS
+  echo ""
+  echo "Downloading RISC-V cross-compiler tarball"
+  rm -f $RISCV_COMPILER_TARBALL
+  /usr/bin/time wget $RISCV_COMPILER_URL
 
-pushd /tmp
-  rm -f $RISC_V_COMPILER_TARBALL
-  /usr/bin/time wget --quiet $RISC_V_COMPILER_URL
-
-  echo "Installing to /usr/local/bin"
-  /usr/bin/time sudo tar --extract \
-    --file $RISC_V_COMPILER_TARBALL \
-    --directory /usr/local \
-    --strip-components=1 \
+  echo "Installing"
+  rm -fr $RISCV_COMPILER_PATH
+  /usr/bin/time tar --extract \
+    --file $RISCV_COMPILER_TARBALL \
     > extract.log 2>&1
 popd
-riscv32-corev-elf-gcc --version
+$RISCV_COMPILER_PATH/bin/riscv32-corev-elf-gcc --version
 
 echo "Install complete"
