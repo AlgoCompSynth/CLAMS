f882a63ae3395e9ec97c96ba9d46efbf41af3c60
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-18 18:29:06 -0700

    no more putting stuff in system directories with sudo!!!

--- PicoSDK/5PicoToolchain.sh
+++ PicoSDK/5PicoToolchain.sh
@@ -19,11 +19,10 @@ pushd $CLAMS_BASE
   mkdir --parents $PICO_PATH
 
   echo ""
-  echo "Cloning Raspberry Pi repositories"
+  echo "Cloning Raspberry Pi Pico SDK repositories"
   pushd $PICO_PATH
 
     git clone --recursive $PICO_SDK_URL
-    git clone --recursive $PICOTOOL_URL
     git clone --recursive $PICO_EXAMPLES_URL
     git clone --recursive $PICO_EXTRAS_URL
     git clone --recursive $PICO_PLAYGROUND_URL
@@ -33,25 +32,31 @@ pushd $CLAMS_BASE
     echo "Listing supported boards to $SUPPORTED_BOARDS"
     ls -l $PICO_PATH/pico-sdk/src/boards/include/boards > $SUPPORTED_BOARDS
 
-    echo ""
-    echo "Installing picotool"
+  popd
 
-    pushd $PICOTOOL_PATH
-      rm -fr build; mkdir build; cd build
-      cmake .. 2>&1 | tee cmake.log
-      make 2>&1 | tee make.log
-      sudo make install
-      picotool version
+  echo ""
+  echo "Cloning Pico utilities"
+  pushd $PICO_UTILITIES_PATH
+    rm -fr $PICOTOOL_REPO_PATH; git clone --recursive $PICOTOOL_URL $PICOTOOL_REPO_PATH
+    rm -fr $OPENOCD_REPO_PATH; git clone --depth=1 $OPENOCD_URL $OPENOCD_REPO_PATH
+
+    echo ""
+    echo "Building picotool"
+    pushd $PICOTOOL_REPO_PATH
+      mkdir build
+      cd build
+      cmake ../
+      make -j`nproc`
+      cp picotool $PICO_UTILITIES_PATH
     popd
 
     echo ""
-    echo Downloading latest debug probe firmware!
-    mkdir --parents $PICO_UTILITIES_PATH
-    pushd $PICO_UTILITIES_PATH
-      curl -sOL \
-        "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe_on_pico.uf2"
-      curl -sOL \
-        "https://github.com/raspberrypi/debugprobe/releases/download/debugprobe-v2.0/debugprobe.uf2"
+    echo "Building openocd"
+    pushd $OPENOCD_REPO_PATH
+      ./bootstrap
+      ./configure --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio --disable-werror
+      make -j`nproc`
+      cp src/openocd $PICO_UTILITIES_PATH
     popd
 
   popd
