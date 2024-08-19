da654a047dbb5af1e7dd3e7a25bb4601e5d5e7ef
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-18 17:13:29 -0700

    add dependencies to build openocd
    Former-commit-id: aff01590f2af14a9dab2695643e6bb4d778d9b5f

--- PicoSDK/4LinuxDeps.sh
+++ PicoSDK/4LinuxDeps.sh
@@ -4,15 +4,22 @@ set -e
 
 echo "Installing base packages"
 sudo apt-get install -y \
+  autoconf \
+  automake \
   build-essential \
   cmake \
+  g++ \
+  gcc \
   gcc-arm-none-eabi \
   gdb-multiarch \
+  libftdi-dev \
   libnewlib-arm-none-eabi \
   libstdc++-arm-none-eabi-newlib \
+  libtool \
   libusb-1.0-0-dev \
   minicom \
-  pkg-config
+  pkg-config \
+  texinfo
 
 ./install-risc-v-compiler.sh
 
