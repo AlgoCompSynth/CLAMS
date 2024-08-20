2b83d4026ef73ad1ca739d349f6a40451f9f6e59
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-20 15:26:23 -0700

    use distro Thonny

--- Python/4LinuxDeps.sh
+++ Python/4LinuxDeps.sh
@@ -5,9 +5,6 @@ set -e
 echo "Installing Linux dependencies"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qqy --no-install-recommends \
-  python3-dev \
-  python3-pip \
-  python3-tk \
-  python3-venv
+  thonny
 
 echo "Finished"
