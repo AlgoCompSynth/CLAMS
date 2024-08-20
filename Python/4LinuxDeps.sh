b109f0e38d734abb9df021626421f14a4ad15560
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-20 11:21:05 -0700

    fix misleading comment

--- Python/4LinuxDeps.sh
+++ Python/4LinuxDeps.sh
@@ -5,12 +5,9 @@ set -e
 echo "Installing Linux dependencies"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qqy --no-install-recommends \
-  libasound2 \
-  libatk-bridge2.0-0 \
-  libgtk-3-0 \
-  libnss3 \
-  libsecret-1-0
-
-./install-arduino-ide.sh
+  python3-dev \
+  python3-pip \
+  python3-tk \
+  python3-venv
 
 echo "Finished"
