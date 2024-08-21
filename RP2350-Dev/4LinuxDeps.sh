89b3c6b23554906f8f111bb1cf3017341f3b48b0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-20 19:34:47 -0700

    uncomment test
    Former-commit-id: d1e4c2146e570091c28865864c94285b29b9c07b

--- RP2350-Dev/4LinuxDeps.sh
+++ RP2350-Dev/4LinuxDeps.sh
@@ -3,26 +3,26 @@
 set -e
 
 echo "Installing Linux dependencies"
-#export DEBIAN_FRONTEND=noninteractive
-#/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
-  #autoconf \
-  #automake \
-  #build-essential \
-  #cmake \
-  #g++ \
-  #gcc \
-  #gdb-multiarch \
-  #libftdi-dev \
-  #libtool \
-  #libusb-1.0-0-dev \
-  #minicom \
-  #pkg-config \
-  #python3-dev \
-  #python3-pip \
-  #python3-venv \
-  #texinfo \
-  #thonny \
-  #> 4LinuxDeps.log 2>&1
+export DEBIAN_FRONTEND=noninteractive
+/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  g++ \
+  gcc \
+  gdb-multiarch \
+  libftdi-dev \
+  libtool \
+  libusb-1.0-0-dev \
+  minicom \
+  pkg-config \
+  python3-dev \
+  python3-pip \
+  python3-venv \
+  texinfo \
+  thonny \
+  > 4LinuxDeps.log 2>&1
 
 ./Installers/install-risc-v-compiler.sh
 ./Installers/install-arm-compiler.sh
