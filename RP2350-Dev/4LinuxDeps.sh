07bcc814995f033264b38383696cfe571471f076
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-20 19:32:53 -0700

    fixed merge problem
    Former-commit-id: 5fd4d854603a398010dd0225842505b4bfe80768

--- RP2350-Dev/4LinuxDeps.sh
+++ RP2350-Dev/4LinuxDeps.sh
@@ -3,27 +3,28 @@
 set -e
 
 echo "Installing Linux dependencies"
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -qqy --no-install-recommends \
-  autoconf \
-  automake \
-  build-essential \
-  cmake \
-  g++ \
-  gcc \
-  gdb-multiarch \
-  libftdi-dev \
-  libtool \
-  libusb-1.0-0-dev \
-  minicom \
-  pkg-config \
-  python3-dev \
-  python3-pip \
-  python3-venv \
-  texinfo \
-  thonny
+#export DEBIAN_FRONTEND=noninteractive
+#/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
+  #autoconf \
+  #automake \
+  #build-essential \
+  #cmake \
+  #g++ \
+  #gcc \
+  #gdb-multiarch \
+  #libftdi-dev \
+  #libtool \
+  #libusb-1.0-0-dev \
+  #minicom \
+  #pkg-config \
+  #python3-dev \
+  #python3-pip \
+  #python3-venv \
+  #texinfo \
+  #thonny \
+  #> 4LinuxDeps.log 2>&1
 
-./install-risc-v-compiler.sh
-./install-arm-compiler.sh
+./Installers/install-risc-v-compiler.sh
+./Installers/install-arm-compiler.sh
 
 echo "Finished"
