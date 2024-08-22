12680ebc901af1bc5aa359226e0a1a7bc269b1f2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-21 18:38:07 -0700

    Python cleaned up - needs tests for Amy!!
    Former-commit-id: 3fdc45dd253f7429134092201f3ce4e0dec83bf9

--- RP2350-Dev/6MicroPython.sh
+++ RP2350-Dev/6MicroPython.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "Setting CLAMS_BASE to $HOME"
-export CLAMS_BASE=$HOME
-
 echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
@@ -15,9 +12,9 @@ mkdir --parents $PICO_PYTHON_PATH
 echo ""
 pushd $PICO_PYTHON_PATH
 
-  echo $PIMORONI_PICO_PLUS2_UF2
+  echo "Downloading $PIMORONI_PICO_PLUS2_UF2"
   curl -sOL $PIMORONI_PICO_PLUS2_URL
-  echo $SPARKFUN_PRO_MICRO_RP2350_UF2
+  echo "Downloading $SPARKFUN_PRO_MICRO_RP2350_UF2"
   curl -sOL $SPARKFUN_PRO_MICRO_RP2350_URL
 
 popd
@@ -26,7 +23,7 @@ popd
 
 echo ""
 echo ""
-echo "Note: 'flash_nuke.uf2' files are now board-specific."
+echo "Note: 'flash_nuke.uf2' files are board-specific."
 echo "You will find them in the appropriate"
 echo ""
 echo "    $PICO_EXAMPLES_PATH/build_*"
