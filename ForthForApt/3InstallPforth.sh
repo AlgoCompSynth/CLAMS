d23cd956daaca29fd82cac042e6cba819da3a7c2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-22 15:24:42 -0700

    clean up comments

--- ForthForApt/3InstallPforth.sh
+++ ForthForApt/3InstallPforth.sh
@@ -10,17 +10,18 @@ pushd $HOME/Projects
 rm -fr pforth*
 /usr/bin/time git clone https://github.com/philburk/pforth.git > /tmp/clone.log 2>&1
 
-echo "Configuring pforth"
 pushd pforth
-rm -fr build; mkdir build; cd build
-cmake ..
 
-echo "Configuring pforth"
-make
+  echo "Configuring pforth"
+  rm -fr build; mkdir build; cd build
+  cmake ..
 
-echo "Installing pforth"
-sudo cp ../fth/pforth_standalone /usr/local/bin/pforth
-echo "bye" | pforth
+  echo "Building pforth"
+  make
+
+  echo "Installing pforth"
+  sudo cp ../fth/pforth_standalone /usr/local/bin/pforth
+  echo "bye" | pforth
 
 popd
 
