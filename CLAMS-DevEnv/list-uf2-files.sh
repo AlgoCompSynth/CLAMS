933c9fc4d0a48a53de85bd9d5037c89c1b11e6f7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-24 15:40:33 -0700

    capture logfile

--- CLAMS-DevEnv/list-uf2-files.sh
+++ CLAMS-DevEnv/list-uf2-files.sh
@@ -6,6 +6,6 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-find $CLAMS_BASE -name '*.uf2' | sort
+find $CLAMS_BASE -name '*.uf2' | sort | tee uf2Files.log
 
 echo "Finished"
