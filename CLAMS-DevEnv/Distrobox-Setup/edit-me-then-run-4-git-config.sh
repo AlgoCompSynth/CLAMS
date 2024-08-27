d64fb7b49c34d3f59bf0cdc99674ee4d0bb97838
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-27 10:29:23 -0700

    assume user knows git stuff

--- CLAMS-DevEnv/Distrobox-Setup/edit-me-then-run-4-git-config.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-git config --global user.email "znmeb@znmeb.net"
-git config --global user.name "M. Edward (Ed) Borasky"
-git config --global advice.detached false
-git config --global pull.rebase false
-git config --global push.default simple
