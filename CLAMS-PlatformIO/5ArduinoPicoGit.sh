2c0258ca7941977f48e7c81d842b76bffa121146
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-26 21:09:32 -0700

    logging boards and examples working

--- CLAMS-PlatformIO/5ArduinoPicoGit.sh
+++ CLAMS-PlatformIO/5ArduinoPicoGit.sh
@@ -2,57 +2,38 @@
 
 set -e
 
-echo "Defining LOGFILE"
-export LOGFILE="$PWD/5ArduinoCLI.log"
+export LOGFILE="$PWD/5ArduinoPicoGit.log"
+echo "LOGFILE: $LOGFILE"
 echo "Removing any existing $HOME/Arduino"
 rm -fr $HOME/Arduino
 
-echo "Installing via 'curl'"
-# https://arduino.github.io/arduino-cli/0.20/installation/
-/usr/bin/time curl -fsSL \
-  https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh \
-  | BINDIR=~/.local/bin sh \
-  > $LOGFILE 2>&1
-
-echo "Creating fresh configuration file"
-# https://arduino.github.io/arduino-cli/0.20/getting-started/
-arduino-cli config init --overwrite
-
-echo "Updating index"
-arduino-cli core update-index
-
-echo "Listing connected boards"
-arduino-cli board list
-
-echo "Installing bash completion file"
-arduino-cli completion bash > /tmp/ardunio-cli.sh
-sudo mv /tmp/ardunio-cli.sh /etc/bash_completion.d/ardunio-cli.sh
-
-echo "Installing zsh completion file"
-arduino-cli completion zsh > /tmp/_arduino-cli.sh
-sudo mv /tmp/_arduino-cli.sh /usr/share/zsh/vendor-completions/_arduino-cli
-
-echo "Installing Arduino MBED RP2040 core"
-arduino-cli core install arduino:mbed_rp2040
-
 # https://arduino-pico.readthedocs.io/en/latest/install.html#installing-via-git
 echo "Installing Arduino Pico from GitHub"
-mkdir -p ~/Arduino/hardware/pico
-/usr/bin/time git clone https://github.com/earlephilhower/arduino-pico.git ~/Arduino/hardware/pico/rp2040 \
+mkdir --parents $HOME/Arduino/hardware/pico
+/usr/bin/time git clone https://github.com/earlephilhower/arduino-pico.git $HOME/Arduino/hardware/pico/rp2040 \
   >> $LOGFILE 2>&1
 
-pushd ~/Arduino/hardware/pico/rp2040
+pushd $HOME/Arduino/hardware/pico/rp2040
+  echo "Updating main submodules"
   /usr/bin/time git submodule update --init \
     >> $LOGFILE 2>&1
+  echo "Updating Pico SDK submodules"
   cd pico-sdk
   /usr/bin/time git submodule update --init \
     >> $LOGFILE 2>&1
+  echo "Installing tools"
   cd ../tools
   /usr/bin/time python3 ./get.py \
     >> $LOGFILE 2>&1
 popd
 
-echo "Listing RP2040 / RP2350 boards to arduino-boards.log"
-arduino-cli board search rp2040 > arduino-boards.log
+echo "Listing Arduino Pico boards to arduino-pico-boards.log"
+arduino-cli board search pico:rp2040 > arduino-pico-boards.log
+
+echo "Listing Arduino Pico examples to arduino-pico-examples.log"
+find $HOME/Arduino/hardware/pico -name 'examples' | sed 's;^.*Arduino/hardware/;;' | sort -u > arduino-pico-examples.log
+
+echo "Listing connected boards"
+arduino-cli board list
 
 echo "Finished"
