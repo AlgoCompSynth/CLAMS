35a022ef1fb8dd6dc8aac83d29e5c50a3f75c77a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-26 18:24:55 -0700

    doc cleanup
    Former-commit-id: 6a7d7ba2c30bbf672850303e70ad14f9e2f1796c

--- CLAMS-PlatformIO/host-nuke-distrobox.sh
+++ CLAMS-PlatformIO/host-nuke-distrobox.sh
@@ -3,14 +3,20 @@
 set -e
 
 export DBX_NAME="CLAMS-PlatformIO"
-echo "Usage: run this script on the Distrobox host"
-echo "to force-remove the distrobox $DBX_NAME"
-echo "and it's home directory $HOME/dbx-homes/$DBX_NAME."
+echo "Run this script on the Distrobox host to force-remove the"
+echo "Distrobox container"
+echo ""
+echo "    $DBX_NAME"
+echo ""
+echo "and its home directory"
+echo ""
+echo "    $HOME/dbx-homes/$DBX_NAME."
+echo ""
 echo ""
 echo "Sleeping 20 seconds - use CTL-C if you change your mind!"
 sleep 20
 
-echo "Removing distrobox $DBX_NAME"
+echo "Removing Distrobox container $DBX_NAME"
 distrobox rm --force $DBX_NAME
 
 echo "Removing $HOME/dbx-homes/$DBX_NAME"
