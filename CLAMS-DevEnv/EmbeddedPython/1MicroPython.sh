6f6ff42b9ceb9555e201193e4c325d0918b99471
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-27 12:10:38 -0700

    separate MicroPython and CircuitPython

--- CLAMS-DevEnv/EmbeddedPython/1MicroPython.sh
+++ CLAMS-DevEnv/EmbeddedPython/1MicroPython.sh
@@ -6,31 +6,34 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo "Creating fresh $PICO_PYTHON_PATH"
-rm -fr $PICO_PYTHON_PATH
-mkdir --parents $PICO_PYTHON_PATH
-echo ""
-pushd $PICO_PYTHON_PATH
+echo "Creating virtual environment $CLAMS_VENV"
+python3 -m venv $CLAMS_VENV
+echo "Activating virtual environment $CLAMS_VENV"
+source $ACTIVATE_CLAMS_VENV
+echo "Installing mpremote"
+pip3 install --upgrade mpremote
+echo "Deactivating virtual environment $CLAMS_VENV"
+deactivate
 
-  echo "Creating virtual environment $CLAMS_VENV"
-  python3 -m venv $CLAMS_VENV
-  echo "Activating virtual environment $CLAMS_VENV"
-  source $ACTIVATE_CLAMS_VENV
-  echo "Installing mpremote and thonny"
-  pip3 install --upgrade mpremote thonny
-  echo "Deactivating virtual environment $CLAMS_VENV"
-  deactivate
+echo "Creating fresh $PICO_MICROPYTHON_PATH"
+rm -fr $PICO_MICROPYTHON_PATH
+mkdir --parents $PICO_MICROPYTHON_PATH
+echo ""
+pushd $PICO_MICROPYTHON_PATH
 
   echo "Downloading $PIMORONI_PICO_PLUS2_UF2"
   curl -sOL $PIMORONI_PICO_PLUS2_URL
+
   echo "Downloading $SPARKFUN_PRO_MICRO_RP2350_UF2"
   curl -sOL $SPARKFUN_PRO_MICRO_RP2350_URL
+  cp $SPARKFUN_PRO_MICRO_RP2350_UF2 micropython_$SPARKFUN_PRO_MICRO_RP2350_UF2
+
   echo "Downloading $PICOVISION_MICROPYTHON_UF2"
   curl -sOL $PICOVISION_MICROPYTHON_URL
+
   echo "Downloading $PICOVISION_WIDESCREEN_MICROPYTHON_UF2"
   curl -sOL $PICOVISION_WIDESCREEN_MICROPYTHON_URL
-  echo "Downloading $PICO_W_CIRCUITPYTHON_UF2"
-  curl -sOL $PICO_W_CIRCUITPYTHON_URL
+
   echo "Downloading $PICO_W_MICROPYTHON_UF2"
   curl -sOL $PICO_W_MICROPYTHON_URL
   cp $PICO_W_MICROPYTHON_UF2 MICROPYTHON_$PICO_W_MICROPYTHON_UF2
@@ -38,9 +41,8 @@ pushd $PICO_PYTHON_PATH
   echo "Downloading $PICOVISION_EXAMPLES_ZIPFILE"
   curl -sOL $PICOVISION_EXAMPLES_URL
   rm -fr $PICOVISION_EXAMPLES_PATH; mkdir --parents $PICOVISION_EXAMPLES_PATH
-
   pushd $PICOVISION_EXAMPLES_PATH
-    unzip $PICO_PYTHON_PATH/$PICOVISION_EXAMPLES_ZIPFILE
+    unzip $PICO_MICROPYTHON_PATH/$PICOVISION_EXAMPLES_ZIPFILE
   popd
 
 popd
