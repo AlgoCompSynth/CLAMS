f48626cb6ff755f4c513d8490194697c2c24935d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-27 12:10:38 -0700

    separate MicroPython and CircuitPython
    Former-commit-id: 6f6ff42b9ceb9555e201193e4c325d0918b99471

---
+++ CLAMS-DevEnv/EmbeddedPython/2CircuitPython.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+
+echo "Creating fresh $PICO_CIRCUITPYTHON_PATH"
+rm -fr $PICO_CIRCUITPYTHON_PATH
+mkdir --parents $PICO_CIRCUITPYTHON_PATH
+echo ""
+pushd $PICO_CIRCUITPYTHON_PATH
+
+  echo "Downloading $PICO_W_CIRCUITPYTHON_UF2"
+  curl -sOL $PICO_W_CIRCUITPYTHON_URL
+
+popd
+
+echo ""
+echo ""
+echo "Note: 'flash_nuke.uf2' files are board-specific."
+echo "You will find them in the appropriate"
+echo ""
+echo "    $PICO_EXAMPLES_PATH/build_*"
+echo ""
+echo "directories."
+echo ""
+echo "Finished"
