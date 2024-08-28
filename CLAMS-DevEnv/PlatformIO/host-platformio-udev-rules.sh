537185f9712efbf5253326b0bc581149e699c60b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-27 17:00:06 -0700

    install Pico udev rule with PlatformIO rules

--- CLAMS-DevEnv/PlatformIO/host-platformio-udev-rules.sh
+++ CLAMS-DevEnv/PlatformIO/host-platformio-udev-rules.sh
@@ -7,6 +7,7 @@ echo "Setting environment variables"
 source ../set_pico_envars
 
 echo "Installing udev rules"
+sudo cp ../99-pico.rules $SYSTEM_UDEV_PATH/
 curl -fsSL $PLATFORMIO_UDEV_RULES_URL | sudo tee $PLATFORMIO_UDEV_RULES_PATH
 sudo udevadm control --reload-rules
 sudo udevadm trigger
