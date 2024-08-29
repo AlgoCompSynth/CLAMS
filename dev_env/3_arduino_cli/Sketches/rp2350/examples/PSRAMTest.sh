921b76e800cb0a3b06bf7a371f8c9c3468cfb601
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-29 14:29:35 -0700

    make port a parameter
    Former-commit-id: 247deac9c69e641e2254ed69ff8852a4d3d30711

--- dev_env/3_arduino_cli/Sketches/rp2350/examples/PSRAMTest.sh
+++ dev_env/3_arduino_cli/Sketches/rp2350/examples/PSRAMTest.sh
@@ -2,7 +2,8 @@
 
 set -e
 
+export PORT=$1
 export FBQN="rp2040:rp2040:sparkfun_promicrorp2350"
 arduino-cli compile --fqbn $FBQN PSRAMTest
-arduino-cli upload --port /dev/ttyACM0 --fqbn $FBQN PSRAMTest
-arduino-cli monitor --port /dev/ttyACM0
+arduino-cli upload --port $PORT --fqbn $FBQN PSRAMTest
+arduino-cli monitor --port $PORT
