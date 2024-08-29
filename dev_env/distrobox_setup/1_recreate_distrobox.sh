fa962b74c011cdd5743855b60f5d6ada32a52fe0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-28 23:58:50 -0700

    fix messed-up container name
    Former-commit-id: ff204030bcaa93e71733b4ea8d7c1ed6c92881f2

--- dev_env/distrobox_setup/1_recreate_distrobox.sh
+++ dev_env/distrobox_setup/1_recreate_distrobox.sh
@@ -5,6 +5,6 @@ set -e
 echo "Creating distrobox"
 distrobox assemble create --replace
 
-echo "Entering CLAMS_devenv - you do *not* have to type the above command!"
+echo "Entering CLAMS_dev_env - you do *not* have to type the above command!"
 echo "This will take some time - it is downloading and installing basic packages."
-distrobox enter CLAMS_devenv
+distrobox enter CLAMS_dev_env
