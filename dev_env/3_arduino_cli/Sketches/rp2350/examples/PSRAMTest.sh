57f3ac7021beeedd2532177f07f5ce511aa7f79f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-08-31 13:24:46 -0700

    don't need PORT envar

--- dev_env/3_arduino_cli/Sketches/rp2350/examples/PSRAMTest.sh
+++ dev_env/3_arduino_cli/Sketches/rp2350/examples/PSRAMTest.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-export PORT=$1
 export FBQN="rp2040:rp2040:sparkfun_promicrorp2350"
 arduino-cli compile --fqbn $FBQN PSRAMTest
-arduino-cli upload --port $PORT --fqbn $FBQN PSRAMTest
-arduino-cli monitor --port $PORT
+arduino-cli upload --port $1 --fqbn $FBQN PSRAMTest
+arduino-cli monitor --port $1
