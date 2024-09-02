b76c09c1bc9abcf673807f3be3c4f248e0953e4e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-01 19:22:55 -0700

    separate MicroPython and PlatformIO venvs
    Former-commit-id: b91df14c67cbd6822ecc098eb052fa2b005f2933

--- dev_env/4_platformio/1_platformio_cli.sh
+++ dev_env/4_platformio/1_platformio_cli.sh
@@ -6,14 +6,16 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo "Activating CLAMS Python virtual environment"
-source $ACTIVATE_CLAMS_VENV
+echo "Creating virtual environment $PLATFORMIO_VENV"
+python3 -m venv $PLATFORMIO_VENV
+echo "Activating virtual environment $PLATFORMIO_VENV"
+source $ACTIVATE_PLATFORMIO_VENV
 
 echo "Installing with pip"
 # https://docs.platformio.org/en/latest/core/installation/methods/pypi.html#installation-pypi
 python3 -m pip install --upgrade platformio
 
-echo "Deactivating CLAMS Python virtual environment"
+echo "Deactivating virtual environment $PLATFORMIO_VENV"
 deactivate
 
 echo "Finished"
