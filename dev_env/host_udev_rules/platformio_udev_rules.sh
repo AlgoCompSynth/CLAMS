ea519a88ea3124d3a9385f0fa3eacb40e8dd12fa
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-02 17:49:27 -0700

    refactored
    Former-commit-id: 9405310eff8835209d1802320e40cb11a38b99e0

--- dev_env/host_udev_rules/platformio_udev_rules.sh
+++ dev_env/host_udev_rules/platformio_udev_rules.sh
@@ -2,15 +2,5 @@
 
 set -e
 
-echo ""
-echo "Setting environment variables"
-source ../set_pico_envars
-
 echo "Installing PlatformIO rules"
 curl -fsSL $PLATFORMIO_UDEV_RULES_URL | sudo tee $PLATFORMIO_UDEV_RULES_PATH
-
-echo "Reloading"
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-
-echo "Finished"
