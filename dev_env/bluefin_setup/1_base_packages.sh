bfba86ad342ec572534de3e75c75150928537ba8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-04 14:19:08 -0700

    standardized to distrobox workflow
    Former-commit-id: ffe1a5fcb4e3e759ba0587e56f898ed241ab6522

--- dev_env/bluefin_setup/1_base_packages.sh
+++ dev_env/bluefin_setup/1_base_packages.sh
@@ -8,20 +8,32 @@ rpm-ostree install --idempotent --allow-inactive \
   automake \
   capstone-devel \
   cmake \
+  file \
+  git-lfs \
   glibc-devel.i686 \
   hidapi-devel \
   libftdi-devel \
   libtool \
   libusb1-devel \
+  lsb-release \
+  lynx \
   make \
   minicom \
   pkgconf \
+  plocate \
   python3-pip \
   screen \
+  speedtest-cli \
   texinfo \
   tmux \
-  2>&1 | tee rpm-ostree.log
+  time \
+  tree \
+  usbutils \
+  vim-enhanced \
+  zsh \
+  2>&1 | tee 1_base_packages.log
 
 echo "Copying 'set_pico_envars' to $HOME"
 cp ../set_pico_envars $HOME/
+
 echo "Finished"
