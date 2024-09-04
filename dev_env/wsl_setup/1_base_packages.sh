01f7e8db76c4055d25bf4885623240918adfa0b9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-04 11:42:31 -0700

    more refactoring

--- dev_env/wsl_setup/1_base_packages.sh
+++ dev_env/wsl_setup/1_base_packages.sh
@@ -3,22 +3,45 @@
 set -e
 
 echo "Installing base packages"
-sudo apt-get update
-sudo apt-get install -y \
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -qq
+sudo apt-get install -qqy \
   apt-file \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
   file \
+  g++ \
+  gcc \
+  gcc-multilib \
+  gdb-multiarch \
   git-lfs \
+  libftdi-dev \
   libpam-systemd \
+  libtool \
+  libusb-1.0-0-dev \
   lsb-release \
   lynx \
+  make \
   man-db \
+  minicom \
+  pkg-config \
   plocate \
+  python3-dev \
+  python3-pip \
+  python3-tk \
+  python3-venv \
+  screen \
   speedtest-cli \
   systemd \
+  texinfo \
   time \
   tmux \
   tree \
+  usbutils \
   vim-nox \
-  zsh
+  zsh \
+  > 1_base_packages.log 2>&1
 
 echo "Finished"
