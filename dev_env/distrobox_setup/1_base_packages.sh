207441d60e2d10c08f0beed41c85405793876569
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-04 13:11:55 -0700

    Refactor the distrobox setup
    Former-commit-id: f8bfbcff31edd5c08a1d0e43b50583240d65645f

---
+++ dev_env/distrobox_setup/1_base_packages.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Installing base packages"
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update -qq
+sudo apt-get install -qqy \
+  apt-file \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  file \
+  g++ \
+  gcc \
+  gcc-multilib \
+  gdb-multiarch \
+  git-lfs \
+  libftdi-dev \
+  libpam-systemd \
+  libtool \
+  libusb-1.0-0-dev \
+  lsb-release \
+  lynx \
+  make \
+  man-db \
+  minicom \
+  pkg-config \
+  plocate \
+  python3-dev \
+  python3-pip \
+  python3-tk \
+  python3-venv \
+  screen \
+  speedtest-cli \
+  systemd \
+  texinfo \
+  time \
+  tmux \
+  tree \
+  usbutils \
+  vim-nox \
+  zsh \
+  > 1_base_packages.log 2>&1
+
+echo "Finished"
