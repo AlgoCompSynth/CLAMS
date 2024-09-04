75ba0855abebcc1562e5ab853425d71e604ecfc8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-03 18:21:06 -0700

    re-create virtual environment

--- dev_env/4_platformio/1_platformio_cli.sh
+++ dev_env/4_platformio/1_platformio_cli.sh
@@ -6,8 +6,9 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo "Creating virtual environment $PLATFORMIO_VENV"
-python3 -m venv $PLATFORMIO_VENV
+echo "Creating fresh virtual environment $PLATFORMIO_VENV"
+rm -fr $PLATFORMIO_VENV
+python3 -m venv $PLATFORMIO_VENV --upgrade-deps
 echo "Activating virtual environment $PLATFORMIO_VENV"
 source $ACTIVATE_PLATFORMIO_VENV
 
