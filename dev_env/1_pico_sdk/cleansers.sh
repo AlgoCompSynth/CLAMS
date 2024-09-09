2ac77520a4f92180ce0aa70e4ae665e39257d9bd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-08 19:53:23 -0700

    archive .uf2, .elf and .dis for debugging
    Former-commit-id: c7c25411eee496642a36278bfe9bab2a52783278

--- dev_env/1_pico_sdk/cleansers.sh
+++ dev_env/1_pico_sdk/cleansers.sh
@@ -8,9 +8,13 @@ source ../set_pico_envars
 
 echo "Creating cleansers.zip"
 pushd $PICO_EXAMPLES_PATH
-  zip -9r cleansers.zip `find . -name "blink.uf2"`
-  zip -9r cleansers.zip `find . -name "hello_serial.uf2"`
-  zip -9r cleansers.zip `find . -name "hello_usb.uf2"`
+  for suffix in uf2 elf dis
+  do
+    zip -9r cleansers.zip `find . -name "blink.$suffix"`
+    zip -9r cleansers.zip `find . -name "flash_nuke.$suffix"`
+    zip -9r cleansers.zip `find . -name "hello_serial.$suffix"`
+    zip -9r cleansers.zip `find . -name "hello_usb.$suffix"`
+  done
   mv cleansers.zip $CLAMS_BASE
 popd
 
