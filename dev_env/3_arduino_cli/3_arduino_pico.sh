917543b735d839a14049249a1081019050af34d6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-09 16:24:16 -0700

    add logging

--- dev_env/3_arduino_cli/3_arduino_pico.sh
+++ dev_env/3_arduino_cli/3_arduino_pico.sh
@@ -5,12 +5,16 @@ set -e
 echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
+export LOGFILE=$PWD/3_arduino_pico.log
 
 # https://arduino-pico.readthedocs.io/en/latest/install.html#installing-via-arduino-cli
 echo "Installing Arduino Pico from package release"
-arduino-cli config add board_manager.additional_urls $ARDUINO_PICO_PACKAGE_URL
-arduino-cli core update-index
-arduino-cli core install rp2040:rp2040
+arduino-cli config add board_manager.additional_urls $ARDUINO_PICO_PACKAGE_URL \
+  >> $LOGFILE 2>&1
+arduino-cli core update-index \
+  >> $LOGFILE 2>&1
+arduino-cli core install rp2040:rp2040 \
+  >> $LOGFILE 2>&1
 
 echo "Listing Arduino Pico boards to arduino_pico_boards.log"
 arduino-cli board listall | grep "rp2040:rp2040" > arduino_pico_boards.log
