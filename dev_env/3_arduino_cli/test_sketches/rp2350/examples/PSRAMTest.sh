ae28811784e1e3012011b338561530ae713c4126
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-09 19:08:09 -0700

    clean up journalctl logic
    Former-commit-id: 4a1ec755c48bf222bcd8428d5ee58bf0f2b990c4

--- dev_env/3_arduino_cli/test_sketches/rp2350/examples/PSRAMTest.sh
+++ dev_env/3_arduino_cli/test_sketches/rp2350/examples/PSRAMTest.sh
@@ -3,30 +3,35 @@
 set -e
 
 echo "Compiling"
-arduino-cli compile --fqbn $FBQN PSRAMTest
+arduino-cli compile --fqbn $FBQN PSRAMTest --log-level trace
 
-echo "USB and TTY status before upload"
+echo "USB status before upload"
 lsusb || true
+echo "TTY status before upload"
 ls -al /dev/ttyACM0 || true
 
 echo "Capturing begin time for 'journalctl' filtering"
-echo "begin `date +%H:%M:%S`" > datestamps.log
+export SINCE=`date +%H:%M:%S`
 
 echo "Sleeping 15 seconds for devices to settle"
 sleep 15
 
 echo "Uploading"
-arduino-cli upload --port /dev/ttyACM0 --fqbn $FBQN PSRAMTest
+arduino-cli upload --port /dev/ttyACM0 --fqbn $FBQN PSRAMTest --log-level trace
 
 echo "Sleeping 15 seconds for devices to settle"
 sleep 15
 
 echo "Capturing end time for 'journalctl' filtering"
-echo "end `date +%H:%M:%S`" >> datestamps.log
+export UNTIL=`date +%H:%M:%S`
+echo "journalctl --dmesg --output=short-precise --since=$SINCE --until=$UNTIL" \
+  > journalctl.sh
 
-echo "USB and TTY status after upload"
+
+echo "USB status after upload"
 lsusb || true
+echo "TTY status after upload"
 ls -al /dev/ttyACM0 || true
 
 echo "Monitoring"
-arduino-cli monitor --port /dev/ttyACM0
+arduino-cli monitor --port /dev/ttyACM0 --config baudrate=115200 --log-level trace
