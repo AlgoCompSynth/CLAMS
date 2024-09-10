4019a4ce28d9ad555ea9c2ff67335ec3be97e312
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-09 17:27:56 -0700

    capture start and stop times for 'journalctl' filtering

--- dev_env/3_arduino_cli/test_sketches/rp2350/examples/PSRAMTest.sh
+++ dev_env/3_arduino_cli/test_sketches/rp2350/examples/PSRAMTest.sh
@@ -2,7 +2,31 @@
 
 set -e
 
-export FBQN="rp2040:rp2040:sparkfun_promicrorp2350"
+echo "Compiling"
 arduino-cli compile --fqbn $FBQN PSRAMTest
-arduino-cli upload --port $1 --fqbn $FBQN PSRAMTest
-arduino-cli monitor --port $1
+
+echo "USB and TTY status before upload"
+lsusb || true
+ls -al /dev/ttyACM0 || true
+
+echo "Capturing begin time for 'journalctl' filtering"
+echo "begin `date +%s`" > datestamps.log
+
+echo "Sleeping 15 seconds for devices to settle"
+sleep 15
+
+echo "Uploading"
+arduino-cli upload --port /dev/ttyACM0 --fqbn $FBQN PSRAMTest
+
+echo "Sleeping 15 seconds for devices to settle"
+sleep 15
+
+echo "Capturing end time for 'journalctl' filtering"
+echo "end `date +%s`" >> datestamps.log
+
+echo "USB and TTY status after upload"
+lsusb || true
+ls -al /dev/ttyACM0 || true
+
+echo "Monitoring"
+arduino-cli monitor --port /dev/ttyACM0
