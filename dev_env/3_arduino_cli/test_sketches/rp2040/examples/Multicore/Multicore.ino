7abaceb1c053be43bd968caa71336d45aa8cf1a0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-10 16:53:20 -0700

    starting over on the examples

--- dev_env/3_arduino_cli/test_sketches/rp2040/examples/Multicore/Multicore.ino
+++ dev_env/3_arduino_cli/test_sketches/rp2040/examples/Multicore/Multicore.ino
@@ -24,17 +24,17 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("C0: Blue leader standing by...\n");
+  Serial.printf("C0: Blue leader standing by...\n");
   delay(1000);
 }
 
 // Running on core1
 void setup1() {
   delay(5000);
-  Serial.print("C1: Red leader standing by...\n");
+  Serial.printf("C1: Red leader standing by...\n");
 }
 
 void loop1() {
-  Serial.print("C1: Stay on target...\n");
+  Serial.printf("C1: Stay on target...\n");
   delay(500);
 }
