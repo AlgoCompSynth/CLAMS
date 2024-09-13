24828e86373b135c49e26bed21bfb960a4b3dff3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-13 13:15:51 -0700

    consolidate compiler installs

--- dev_env/1_pico_sdk/1_install_compilers.sh
+++ dev_env/1_pico_sdk/1_install_compilers.sh
@@ -6,7 +6,38 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-./install_arm_compiler.sh*
-./install_risc_v_compiler.sh*
+pushd /tmp
+
+  echo ""
+  echo "Downloading Arm cross-compiler tarball"
+  rm -f $ARM_COMPILER_TARBALL
+  /usr/bin/time curl -sOL $ARM_COMPILER_URL
+
+  echo "Installing"
+  mkdir --parents $ARM_COMPILER_PATH
+  /usr/bin/time tar --extract \
+    --file $ARM_COMPILER_TARBALL \
+    --strip-components=1 \
+    --directory=$ARM_COMPILER_PATH \
+    > arm-extract.log 2>&1
+  $ARM_COMPILER_PATH/bin/arm-none-eabi-gcc --version
+  echo "Install complete"
+
+  echo ""
+  echo "Downloading RISC-V cross-compiler tarball"
+  rm -f $RISCV_COMPILER_TARBALL
+  /usr/bin/time curl -sOL $RISCV_COMPILER_URL
+
+  echo "Installing"
+  mkdir --parents $RISCV_COMPILER_PATH
+  /usr/bin/time tar --extract \
+    --file $RISCV_COMPILER_TARBALL \
+    --strip-components=1 \
+    --directory=$RISCV_COMPILER_PATH \
+    > risc-v-extract.log 2>&1
+  $RISCV_COMPILER_PATH/bin/riscv32-corev-elf-gcc --version
+  echo "Install complete"
+
+popd
 
 echo "Finished"
