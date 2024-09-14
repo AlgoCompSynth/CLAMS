f88395f1cc146f003d770f9aab34424b331dcc4f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-13 20:36:58 -0700

    make some space
    Former-commit-id: d94c5ad1f922e94244eef774fb0bb54d41cd26ff

--- dev_env/distrobox_setup/0_recreate_distrobox.sh
+++ dev_env/distrobox_setup/0_recreate_distrobox.sh
@@ -7,4 +7,6 @@ distrobox assemble create --replace
 
 echo "Entering CLAMS_dev_env - you do *not* have to type the above command!"
 echo "This will take some time - it is downloading and installing basic packages."
+echo ""
+echo ""
 distrobox enter CLAMS_dev_env
