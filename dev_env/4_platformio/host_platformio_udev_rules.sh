8ce14ed4539ebc0846207e5502d8b8b8af82098a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-13 22:05:31 -0700

    fix missing envar setting!!

--- dev_env/4_platformio/host_platformio_udev_rules.sh
+++ dev_env/4_platformio/host_platformio_udev_rules.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+
 echo "Installing PlatformIO rules"
 curl -fsSL $PLATFORMIO_UDEV_RULES_URL | sudo tee $PLATFORMIO_UDEV_RULES_PATH
 
