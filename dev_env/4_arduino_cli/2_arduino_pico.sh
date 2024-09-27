755e23efdb369fe3fa36324e468e238894458009
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-27 13:18:52 -0700

    clean up board listing bugs
    Former-commit-id: c8429830ad3d45fcbe0fb2366d25e4a4294b0b56

--- dev_env/4_arduino_cli/2_arduino_pico.sh
+++ dev_env/4_arduino_cli/2_arduino_pico.sh
@@ -17,8 +17,8 @@ arduino-cli core install rp2040:rp2040 \
   >> $LOGFILE 2>&1
 arduino-cli core list
 
-echo "Listing Arduino Pico boards to arduino_pico_boards.log"
-arduino-cli board listall | grep "rp2040:rp2040" > arduino_pico_boards.log
+echo "Listing Arduino Pico boards to board-listall.txt"
+arduino-cli board listall > board-listall.txt
 
 echo "Listing Arduino Pico examples to arduino_pico_examples.log"
 find $HOME/.arduino15/packages/rp2040/hardware/rp2040 -name 'examples' \
