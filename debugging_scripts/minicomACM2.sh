b8e40cb18a73ef84883609323fb1d461b443902b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-01 10:07:37 -0700

    add bootsel script and refactor
    Former-commit-id: 0886e6689024729b9c062da55a6338bedb9658e5

--- debugging_scripts/minicomACM2.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-echo "Starting minicom"
-ls -al /dev/ttyA*
-sleep 5
-minicom -b 115200 -o -D /dev/ttyACM2
--- debugging_scripts/minicomUSB0.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-echo "Starting minicom"
-ls -al /dev/ttyU*
-sleep 5
-minicom -b 115200 -o -D /dev/ttyUSB0
