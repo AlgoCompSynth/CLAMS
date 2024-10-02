e83af30517bfce29e93d9de16b13861c7141ba53
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-02 13:41:59 -0700

    move CircuitPython URLs out of set_pico_envars
    Former-commit-id: 45a1c0f7ed29d2594ade7d17c46182c034447035

--- dev_env/embedded_python/2_circuitpython.sh
+++ dev_env/embedded_python/2_circuitpython.sh
@@ -12,14 +12,21 @@ mkdir --parents $PICO_CIRCUITPYTHON_PATH
 echo ""
 pushd $PICO_CIRCUITPYTHON_PATH
 
-  echo "Downloading $PICO_CIRCUITPYTHON_UF2"
-  curl -sOL $PICO_CIRCUITPYTHON_URL
-  echo "Downloading $PICO_W_CIRCUITPYTHON_UF2"
-  curl -sOL $PICO_W_CIRCUITPYTHON_URL
-  echo "Downloading $PICO2_CIRCUITPYTHON_UF2"
-  curl -sOL $PICO2_CIRCUITPYTHON_URL
-  echo "Downloading $SPARKFUN_RP2350_CIRCUITPYTHON_UF2"
-  curl -sOL $SPARKFUN_RP2350_CIRCUITPYTHON_URL
+  export BASE_URL="https://downloads.circuitpython.org/bin"
+  export VERSION="9.2.0-beta.0"
+  for uf2 in \
+    "adafruit_feather_rp2350/en_US/adafruit-circuitpython-adafruit_feather_rp2350-en_US-$VERSION.uf2" \
+    "sparkfun_pro_micro_rp2350/en_US/adafruit-circuitpython-sparkfun_pro_micro_rp2350-en_US-$VERSION.uf2" \
+    "raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-$VERSION.uf2" \
+    "raspberry_pi_pico_w/en_US/adafruit-circuitpython-raspberry_pi_pico_w-en_US-$VERSION.uf2" \
+    "arduino_nano_rp2040_connect/en_US/adafruit-circuitpython-arduino_nano_rp2040_connect-en_US-$VERSION.uf2" \
+    "pimoroni_pico_dv_base/en_US/adafruit-circuitpython-pimoroni_pico_dv_base-en_US-$VERSION.uf2" \
+    "pimoroni_pico_dv_base_w/en_US/adafruit-circuitpython-pimoroni_pico_dv_base_w-en_US-$VERSION.uf2" \
+    "pimoroni_keybow2040/en_US/adafruit-circuitpython-pimoroni_keybow2040-en_US-$VERSION.uf2"
+  do
+    echo "Downloading $BASE_URL/$uf2"
+    curl -sOL $BASE_URL/$uf2
+  done
 
 popd
 
