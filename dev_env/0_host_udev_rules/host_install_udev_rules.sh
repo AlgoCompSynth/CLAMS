88723db936b5322c6a2acfd1e29d48ae989dbb7d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-05 11:57:42 -0700

    Teensy udev rules added
    Former-commit-id: 46bce31b206fe20a353d3f80bc47ab1a6fa68cad

--- dev_env/0_host_udev_rules/host_install_udev_rules.sh
+++ dev_env/0_host_udev_rules/host_install_udev_rules.sh
@@ -6,7 +6,7 @@ echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
 
-echo "Installing picotool udev rules"
+echo "Installing picotool udev rules as 'root'!!"
 sudo cp 99-picotool.rules $SYSTEM_UDEV_PATH/
 
 echo "Installing Arduino MBED RP2040 udev rules as 'root'!!"
@@ -18,9 +18,8 @@ sudo cp 60-openocd.rules $SYSTEM_UDEV_PATH/
 echo "Installing apio FPGA udev rules as 'root'!!"
 sudo cp 80-fpga-*.rules $SYSTEM_UDEV_PATH/
 
-echo ""
-echo "Setting environment variables"
-source ../set_pico_envars
+echo "Installing teensy udev rules as 'root'!!"
+sudo cp 00-teensy.rules $SYSTEM_UDEV_PATH/
 
 echo "Installing PlatformIO rules"
 curl -fsSL $PLATFORMIO_UDEV_RULES_URL | sudo tee $PLATFORMIO_UDEV_RULES_PATH
