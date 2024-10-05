279fce22133a7792f5789056513d729d9eb895c0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-05 15:33:19 -0700

    add DaisyDuino
    Former-commit-id: 61452f2b0d1adbe3ca76d226dd0d976a307261f4

--- dev_env/arduino_cli/boards_and_examples.sh
+++ dev_env/arduino_cli/boards_and_examples.sh
@@ -15,6 +15,9 @@ arduino-cli core list > cores.txt
 echo "Listing boards to boards.txt"
 arduino-cli board listall > boards.txt
 
+echo "Listing libraries to libraries.txt"
+arduino-cli lib list > libraries.txt
+
 echo "Listing examples to examples.txt"
 arduino-cli lib examples > examples.txt
 
