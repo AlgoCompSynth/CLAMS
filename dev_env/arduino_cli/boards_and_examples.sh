8e37ace48b5fb9346040d128641cf43b5e114082
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-05 17:14:37 -0700

    no color on examples

--- dev_env/arduino_cli/boards_and_examples.sh
+++ dev_env/arduino_cli/boards_and_examples.sh
@@ -10,15 +10,15 @@ echo "Updating indexes"
 arduino-cli update
 
 echo "Listing cores to cores.txt"
-arduino-cli core list > cores.txt
+arduino-cli core list --no-color > cores.txt
 
 echo "Listing boards to boards.txt"
-arduino-cli board listall > boards.txt
+arduino-cli board listall --no-color > boards.txt
 
 echo "Listing libraries to libraries.txt"
-arduino-cli lib list > libraries.txt
+arduino-cli lib list --no-color > libraries.txt
 
 echo "Listing examples to examples.txt"
-arduino-cli lib examples > examples.txt
+arduino-cli lib examples --no-color > examples.txt
 
 echo "Finished"
