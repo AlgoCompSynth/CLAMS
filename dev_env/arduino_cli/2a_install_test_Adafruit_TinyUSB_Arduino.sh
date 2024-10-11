036b78aa4a5f22c6ecca908dd479c614813439aa
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-10 18:11:30 -0700

    add script to test Arduino USB audio
    Former-commit-id: 2f57e58c5b8a5278bff09dca117fb4322e7915f8

---
+++ dev_env/arduino_cli/2a_install_test_Adafruit_TinyUSB_Arduino.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "Setting environment variables"
+source ../set_pico_envars
+export LOGFILE=$PWD/test_Adafruit_TinyUSB_Arduino.log
+rm --force $LOGFILE
+
+# https://github.com/pschatzmann/Adafruit_TinyUSB_Arduino/discussions/1
+echo "Installing Adafruit_TinyUSB_Arduino from GitHub"
+pushd $ADAFRUIT_TINYUSB_ARDUINO_PATH/..
+  rm --force --recursive $ADAFRUIT_TINYUSB_ARDUINO_PATH
+  /usr/bin/time git clone $ADAFRUIT_TINYUSB_ARDUINO_URL \
+    >> $LOGFILE 2>&1
+popd
+
+echo "Finished"
