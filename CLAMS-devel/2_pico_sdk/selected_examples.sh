9eb2897b3e0420aa71585b1bc17beae2cb21624b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-10-12 19:14:47 -0700

    add sine_wave examples from pico-playground

--- CLAMS-devel/2_pico_sdk/selected_examples.sh
+++ CLAMS-devel/2_pico_sdk/selected_examples.sh
@@ -5,25 +5,39 @@ set -e
 echo ""
 echo "Setting environment variables"
 source ../set_pico_envars
+export LOGFILE=$PWD/selected_examples.log
+rm --force $LOGFILE
 
-echo "Creating selected_examples.zip"
+echo "Creating $CLAMS_BASE/selected_examples.zip"
 pushd $PICO_EXAMPLES_PATH
   for suffix in uf2 elf dis
   do
-    zip -9r selected_examples.zip `find . -name "blink.$suffix"`
-    zip -9r selected_examples.zip `find . -name "flash_nuke.$suffix"`
-    zip -9r selected_examples.zip `find . -name "hello_serial.$suffix"`
-    zip -9r selected_examples.zip `find . -name "hello_usb.$suffix"`
-    zip -9r selected_examples.zip `find . -name "tinyusb*.$suffix"`
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "blink.$suffix"` \
+      >> $LOGFILE 2>&1
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "flash_nuke.$suffix"` \
+      >> $LOGFILE 2>&1
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_serial.$suffix"` \
+      >> $LOGFILE 2>&1
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "hello_usb.$suffix"` \
+      >> $LOGFILE 2>&1
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "tinyusb*.$suffix"` \
+      >> $LOGFILE 2>&1
   done
-  mv selected_examples.zip $CLAMS_BASE
+popd
 
-  pushd $CLAMS_BASE
-    echo "Unpacking selected_examples.zip"
-    rm -fr build_*
-    unzip selected_examples.zip
-  popd
+pushd $PICO_PLAYGROUND_PATH
+  for suffix in uf2 elf dis
+  do
+    /usr/bin/time zip -9r $CLAMS_BASE/selected_examples.zip `find . -name "sine_wave*.$suffix"` \
+      >> $LOGFILE 2>&1
+  done
+popd
 
+echo "Unpacking $CLAMS_BASE/selected_examples.zip"
+pushd $CLAMS_BASE
+  rm -fr build_*
+  /usr/bin/time unzip selected_examples.zip \
+    >> $LOGFILE 2>&1
 popd
 
 echo "Finished"
